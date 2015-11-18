e86ace7bfdff8e367e64a9376bb817bb07f79ac7
Robert Arnold <rarnoldmobile@gmail.com>
2015-11-17 21:11:33 -0500

    Test

---
+++ attribute-storm/src/main/java/com/enforce/detection/attributeCollection/App.java
@@ -0,0 +1,13 @@
+package com.enforce.detection.attributeCollection;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ web/src/main/java/com/enforce/detection/web/App.java
@@ -0,0 +1,13 @@
+package com.enforce.detection.web;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
